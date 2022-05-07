c3af75925e7e9bea497e684ec3589242ff4aa55c
Julian Metzler <git@mezgr.de>
2022-05-08 00:17:40 +0200

    16seg LED: Update rendering to handle decimal points correctly

--- components/driver_display_char_16seg_led_spi/char_16seg_led_spi.c
+++ components/driver_display_char_16seg_led_spi/char_16seg_led_spi.c
@@ -124,25 +124,81 @@ void display_latch() {
     gpio_pulse_inv(CONFIG_16SEG_LED_LATCH_IO, 1, CONFIG_16SEG_LED_LATCH_PULSE_LENGTH, CONFIG_16SEG_LED_LATCH_PULSE_LENGTH, CONFIG_16SEG_LED_LATCH_INV);
 }
 
+uint16_t display_calculateFrameBufferCharacterIndex(uint16_t charPos) {
+    // Calculate the frame buffer start index for the given character position
+    // Position starts at 0 in the top left corner
+    uint16_t fb_index = 0;
+    fb_index = CONFIG_DISPLAY_FONT_BYTES_PER_CHAR * (DISPLAY_CHARBUF_SIZE - charPos - 1);
+    #if defined(CONFIG_16SEG_LED_USE_DP_DATA)
+        fb_index += (DISPLAY_CHARBUF_SIZE - charPos - 1) / CONFIG_16SEG_LED_DP_DATA_INTERVAL;
+    #endif
+    return fb_index;
+}
+
+#if defined(CONFIG_16SEG_LED_USE_DP_DATA)
+uint16_t display_calculateFrameBufferDecimalPointIndex(uint16_t charPos) {
+    // Calculate the frame buffer start index for the decimal point for the given character position
+    // Position starts at 0 in the top left corner
+    return (((DISPLAY_CHARBUF_SIZE - charPos - 1) / CONFIG_16SEG_LED_DP_DATA_INTERVAL) * (CONFIG_DISPLAY_FONT_BYTES_PER_CHAR * CONFIG_16SEG_LED_DP_DATA_INTERVAL + 1)) + (CONFIG_DISPLAY_FONT_BYTES_PER_CHAR * CONFIG_16SEG_LED_DP_DATA_INTERVAL);
+}
+
+uint8_t display_calculateDecimalPointSubIndex(uint16_t charPos) {
+    // Calculate the bit position for the decimal point for the given character position
+    // Position starts at 0 in the top left corner
+    return charPos % CONFIG_16SEG_LED_DP_DATA_INTERVAL;
+}
+
+void display_setDecimalPointAt(uint8_t* frameBuf, uint16_t charPos, uint8_t state) {
+    if (state) {
+        frameBuf[display_calculateFrameBufferDecimalPointIndex(charPos)] |= (1 << display_calculateDecimalPointSubIndex(charPos));
+    } else {
+        frameBuf[display_calculateFrameBufferDecimalPointIndex(charPos)] &= ~(1 << display_calculateDecimalPointSubIndex(charPos));
+    }
+}
+#endif
+
+void display_setCharDataAt(uint8_t* frameBuf, uint16_t charPos, uint16_t charData) {
+    uint16_t fb_base_i = display_calculateFrameBufferCharacterIndex(charPos);
+    for (uint8_t i = 0; i < CONFIG_DISPLAY_FONT_BYTES_PER_CHAR; i++) {
+        frameBuf[fb_base_i + i] = (charData >> (8 * (CONFIG_DISPLAY_FONT_BYTES_PER_CHAR - i - 1))) & 0xFF;
+    }
+}
+
 /*
  TODO
- - Implement dot handling
  - Add brightness control (also in canvas interface)
  - Display multiline text field in canvas
  */
 
 void display_charbuf_to_framebuf(uint8_t* charBuf, uint8_t* frameBuf, uint16_t charBufSize, uint16_t frameBufSize) {
-    uint16_t fb_i = 0;
-    uint8_t charData;
+    uint8_t prevWasLetter = 0;
+    uint16_t decPointMergeCnt = 0;
+    uint16_t cb_i_display = 0;
+
     memset(frameBuf, 0x00, frameBufSize);
-    for (uint16_t cb_i = 0; cb_i < charBufSize; cb_i++) {
-        fb_i = 2 * (charBufSize - cb_i - 1) + (charBufSize - cb_i - 1) / 8;
-        for (uint8_t c = 0; c < CONFIG_DISPLAY_FONT_BYTES_PER_CHAR; c++) {
-            if (fb_i + c >= frameBufSize) break;
-            if (charBuf[cb_i] < char_seg_font_min || charBuf[cb_i] > char_seg_font_max) continue;
-            charData = (uint8_t)(char_16seg_font[charBuf[cb_i] - char_seg_font_min] >> ((CONFIG_DISPLAY_FONT_BYTES_PER_CHAR - c - 1) * 8));
-            frameBuf[fb_i + c] = charData;
+
+    for (uint16_t cb_i_source = 0; cb_i_source < charBufSize; cb_i_source++) {
+        if (charBuf[cb_i_source] == '.') {
+            if (!prevWasLetter) {
+                // Current char is . and previous was . too
+                display_setDecimalPointAt(frameBuf, cb_i_display, 1);
+            } else {
+                // Current char is . but previous was a letter
+                // Add the point to the previous letter and update the respective counter
+                cb_i_display--;
+                display_setDecimalPointAt(frameBuf, cb_i_display, 1);
+                decPointMergeCnt++;
+                prevWasLetter = 0;
+            }
+        } else {
+            // Current char is a letter
+            if (charBuf[cb_i_source] >= char_seg_font_min && charBuf[cb_i_source] <= char_seg_font_max) {
+                display_setCharDataAt(frameBuf, cb_i_display, char_16seg_font[charBuf[cb_i_source] - char_seg_font_min]);
+            }
+            prevWasLetter = 1;
         }
+
+        cb_i_display++;
     }
 }
 
