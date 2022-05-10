12612ba82f89ad624620f98dcd54402c0ff601ac
Julian Metzler <git@mezgr.de>
2022-05-10 19:20:31 +0200

    Update path in pre-build script (Why did this suddenly stop working? How did it even work before?)

--- pre_build.py
+++ pre_build.py
@@ -1,6 +1,6 @@
 import shutil
 
 try:
-    shutil.rmtree(".pio/build/esp-wrover-kit/esp-idf/Browser_OTA")
+    shutil.rmtree(".pio/build/esp-wrover-kit/components/Browser_OTA")
 except FileNotFoundError:
     pass
