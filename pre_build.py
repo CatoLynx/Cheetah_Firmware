bf64fb185b6748db8e62ca7275bc0845749a4077
Julian Metzler <git@mezgr.de>
2022-10-14 14:26:15 +0200

    PIO: Set up base env + envs for build variants to use separate sdkconfigs

--- pre_build.py
+++ pre_build.py
@@ -1,5 +1,6 @@
 import shutil
 
+print("Removing compiled Browser_OTA component to update compile date and time")
 try:
     shutil.rmtree(".pio/build/esp-wrover-kit/components/Browser_OTA")
 except FileNotFoundError:
