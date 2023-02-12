CatoLynx/Cheetah_Firmware

commit 7f3ed037a954fcbc68fa3d1ea460e549427d7718

Add WS281x driver and pin tasks to cores

include/macros.h
@@ -36,7 +36,7 @@
     #define DISPLAY_TYPE "character"
     #define DISPLAY_FRAME_TYPE ""
     #define DISPLAY_CHARBUF_SIZE (CONFIG_DISPLAY_FRAME_WIDTH * CONFIG_DISPLAY_FRAME_HEIGHT)
-    #define DISPLAY_FRAMEBUF_SIZE (DIV_CEIL(DISPLAY_CHARBUF_SIZE * CONFIG_DISPLAY_FRAMEBUF_BITS_PER_CHAR, 8))
+    #define DISPLAY_FRAMEBUF_SIZE (DISPLAY_CHARBUF_SIZE * DIV_CEIL(CONFIG_DISPLAY_FRAMEBUF_BITS_PER_CHAR, 8))
 #endif
 
 #if defined(CONFIG_DISPLAY_DRIVER_FLIPDOT_LAWO_ALUMA)
@@ -57,4 +57,6 @@
     #define DISPLAY_DRIVER "char_16seg_led_spi"
 #elif defined(CONFIG_DISPLAY_DRIVER_CHAR_KRONE_9000)
     #define DISPLAY_DRIVER "char_krone_9000"
+#elif defined(CONFIG_DISPLAY_DRIVER_CHAR_16SEG_LED_WS281X)
+    #define DISPLAY_DRIVER "char_16seg_led_ws281x"
 #endif

components/browser_ota/browser_ota.c
@@ -267,7 +267,7 @@ void browser_ota_init(httpd_handle_t* server) {
     ota_server = server;
     
     ESP_LOGI(LOG_TAG, "Creating restart task");
-    xTaskCreate(&systemRestartTask, "restartTask", 2048, NULL, 5, &systemRestartTaskHandle);
+    xTaskCreatePinnedToCore(&systemRestartTask, "restartTask", 2048, NULL, 5, &systemRestartTaskHandle, 0);
 }
 
 void browser_ota_deinit(void) {

components/driver_display_char_16seg_led_ws281x/CMakeLists.txt
@@ -1,0 +1,3 @@
+idf_component_register(SRCS         char_16seg_led_ws281x.c led_strip_rmt_ws2812.c
+                       INCLUDE_DIRS include
+                       REQUIRES     util)

components/driver_display_char_16seg_led_ws281x/Kconfig
@@ -1,0 +1,20 @@
+menu "16-segment WS281x Configuration"
+
+config 16SEG_WS281X_DATA_IO
+    int "Data GPIO"
+    default 0
+    help
+        GPIO Pin for LED data
+
+choice 16SEG_WS281X_SPI_HOST
+    bool "SPI Host"
+    default 16SEG_WS281X_SPI_HOST_VSPI
+
+    config 16SEG_WS281X_SPI_HOST_VSPI
+        bool "VSPI"
+    
+    config 16SEG_WS281X_SPI_HOST_HSPI
+        bool "HSPI"
+endchoice
+
+endmenu

components/driver_display_char_16seg_led_ws281x/char_16seg_font.h
@@ -1,0 +1,148 @@
+#include <stdint.h>
+
+const uint8_t char_seg_font_min = 32;
+const uint8_t char_seg_font_max = 127;
+
+/*
+
+  SEGMENT MAPPING
+
+  Seg | Bit
+  a   | 1000000000000000
+  b   | 0000000000000001
+  c   | 0000000000000100
+  d   | 0000000000100000
+  e   | 0000000001000000
+  f   | 0000000010000000
+  g   | 0000000100000000
+  h   | 0001000000000000
+  k   | 0010000000000000
+  m   | 0100000000000000
+  n   | 0000000000000010
+  p   | 0000000000001000
+  r   | 0000000000010000
+  s   | 0000010000000000
+  t   | 0000001000000000
+  u   | 0000100000000000
+
+*/
+
+typedef enum SEGMENTS {
+  A = 32768,
+  B = 1,
+  C = 4,
+  D = 32,
+  E = 64,
+  F = 128,
+  G = 256,
+  H = 4096,
+  K = 8192,
+  M = 16384,
+  N = 2,
+  P = 8,
+  R = 16,
+  S = 1024,
+  T = 512,
+  U = 2048,
+  DP = 65536
+} seg_t;
+
+// Starts at dec 32 (Space)
+const uint32_t char_16seg_font[96] = {
+  /*   */ 0,
+  /* ! */ A | B | E | F | K | M | N,
+  /* " */ C | H,
+  /* # */ C | D | E | F | M | P | S | U,
+  /* $ */ A | B | D | E | F | H | M | P | S | U,
+  /* % */ A | E | N | T,
+  /* & */ A | E | F | G | H | M | P | R | U,
+  /* ' */ M,
+  /* ( */ A | F | G | H,
+  /* ) */ B | C | D | E,
+  /* * */ K | M | N | P | R | S | T | U,
+  /* + */ M | P | S | U,
+  /* , */ T,
+  /* - */ P | U,
+  /* . */ DP,
+  /* / */ N | T,
+  /* 0 */ A | B | C | D | E | F | G | H | N | T,
+  /* 1 */ C | D | N,
+  /* 2 */ A | B | C | E | F | G | P | U,
+  /* 3 */ A | B | C | D | E | F | P | U,
+  /* 4 */ C | D | H | P | U,
+  /* 5 */ A | B | D | E | F | H | P | U,
+  /* 6 */ A | B | D | E | F | G | H | P | U,
+  /* 7 */ A | B | C | D,
+  /* 8 */ A | B | C | D | E | F | G | H | P | U,
+  /* 9 */ A | B | C | D | E | F | H | P | U,
+  /* : */ F | U,
+  /* ; */ A | T,
+  /* < */ N | R,
+  /* = */ E | F | P | U,
+  /* > */ K | T,
+  /* ? */ A | B | C | P | S,
+  /* @ */ A | B | C | D | E | G | H | P | S,
+  /* A */ A | B | C | D | G | H | P | U,
+  /* B */ A | B | C | D | E | F | M | P | S,
+  /* C */ A | B | E | F | G | H,
+  /* D */ A | B | C | D | E | F | M | S,
+  /* E */ A | B | E | F | G | H | U,
+  /* F */ A | B | G | H | U,
+  /* G */ A | B | D | E | F | G | H | P,
+  /* H */ C | D | G | H | P | U,
+  /* I */ A | B | E | F | M | S,
+  /* J */ C | D | E | F,
+  /* K */ G | H | N | R | U,
+  /* L */ E | F | G | H,
+  /* M */ C | D | G | H | K | N,
+  /* N */ C | D | G | H | K | R,
+  /* O */ A | B | C | D | E | F | G | H,
+  /* P */ A | B | C | G | H | P | U,
+  /* Q */ A | B | C | D | E | F | G | H | R,
+  /* R */ A | B | C | G | H | P | R | U,
+  /* S */ A | B | D | E | F | H | P | U,
+  /* T */ A | B | M | S,
+  /* U */ C | D | E | F | G | H,
+  /* V */ G | H | N | T,
+  /* W */ C | D | G | H | R | T,
+  /* X */ K | N | R | T,
+  /* Y */ K | N | S,
+  /* Z */ A | B | E | F | N | T,
+  /* [ */ B | E | M | S,
+  /* \ */ K | R,
+  /* ] */ A | F | M | S,
+  /* ^ */ H | K,
+  /* _ */ E | F,
+  /* ` */ K,
+  /* a */ E | F | G | S | U,
+  /* b */ D | E | F | G | H | P | U,
+  /* c */ E | F | G | P | U,
+  /* d */ C | D | E | F | G | P | U,
+  /* e */ A | B | C | E | F | G | H | P | U,
+  /* f */ B | M | P | S | U,
+  /* g */ B | C | D | E | M | P,
+  /* h */ D | G | H | P | U,
+  /* i */ A | B | S,
+  /* j */ A | B | F | S,
+  /* k */ G | H | N | R | U,
+  /* l */ G | H,
+  /* m */ D | G | P | S | U,
+  /* n */ D | G | P | U,
+  /* o */ D | E | F | G | P | U,
+  /* p */ A | G | H | M | U,
+  /* q */ B | C | D | M | P,
+  /* r */ G | U,
+  /* s */ E | P | R,
+  /* t */ F | G | H | U,
+  /* u */ D | E | F | G,
+  /* v */ G | T,
+  /* w */ D | G | R | T,
+  /* x */ K | N | R | T,
+  /* y */ F | K | N | S,
+  /* z */ F | T | U,
+  /* { */ N | R | U,
+  /* | */ M | S,
+  /* } */ K | P | T,
+  /* ~ */ P | U,
+  /* DEL */ A | B | C | D | E | F | G | H | K | M | N | P | R | S | T | U
+};

components/driver_display_char_16seg_led_ws281x/char_16seg_led_ws281x.c
@@ -1,0 +1,257 @@
+/*
+ * Functions for WS281x based LED displays
+ */
+
+#include "driver/spi_master.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <string.h>
+
+#include "macros.h"
+#include "char_16seg_led_ws281x.h"
+#include "util_generic.h"
+#include "util_gpio.h"
+#include "char_16seg_font.h"
+
+#if defined(CONFIG_DISPLAY_DRIVER_CHAR_16SEG_LED_WS281X)
+
+
+#define LOG_TAG "CH-16SEG-LED-WS281X"
+
+spi_device_handle_t spi;
+volatile uint8_t display_transferOngoing = false;
+uint8_t display_currentBrightness = 255;
+static const color_t OFF = { 0, 0, 0 };
+
+static const uint8_t ws281x_bit_patterns[4] = {
+    0x88,
+	0x8E,
+	0xE8,
+	0xEE,
+};
+
+
+void display_init() {
+    /*
+     * Set up all needed peripherals
+     */
+
+    // Init SPI peripheral
+    spi_bus_config_t buscfg = {
+        .miso_io_num = -1,
+        .mosi_io_num = CONFIG_16SEG_WS281X_DATA_IO,
+        .sclk_io_num = -1,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = DISPLAY_FRAMEBUF_SIZE
+    };
+    spi_device_interface_config_t devcfg = {
+        .clock_speed_hz = 3200000UL,    // 3.2 MHz gives correct timings
+        .mode = 0,                      // positive clock, sample on rising edge
+        .spics_io_num = -1,             // -1 = not used
+        .queue_size = 1,                // max. 1 transaction in queue
+        .pre_cb = display_pre_transfer_cb,
+        .post_cb = display_post_transfer_cb,
+    };
+
+    #if defined(CONFIG_16SEG_WS281X_SPI_HOST_VSPI)
+    ESP_ERROR_CHECK(spi_bus_initialize(VSPI_HOST, &buscfg, 1));
+    ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &devcfg, &spi));
+    #elif defined(CONFIG_16SEG_WS281X_SPI_HOST_HSPI)
+    ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, 1));
+    ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &spi));
+    #endif
+}
+
+void display_pre_transfer_cb(spi_transaction_t *t) {
+    display_transferOngoing = true;
+}
+
+void display_post_transfer_cb(spi_transaction_t *t) {
+    display_transferOngoing = false;
+}
+
+#if defined(CONFIG_DISPLAY_HAS_BRIGHTNESS_CONTROL)
+esp_err_t display_set_brightness(uint8_t brightness) {
+    display_currentBrightness = brightness;
+    return ESP_OK;
+}
+#else
+esp_err_t display_set_brightness(uint8_t brightness) {
+    return ESP_OK;
+}
+#endif
+
+uint32_t display_calculateFrameBufferCharacterIndex(uint16_t charPos) {
+    // Calculate the frame buffer start index for the given character position
+    // Position starts at 0 in the top left corner
+    uint32_t fb_index = 0;
+    fb_index = DIV_CEIL(CONFIG_DISPLAY_FRAMEBUF_BITS_PER_CHAR, 8) * charPos;
+    return fb_index;
+}
+
+void display_setLEDColor(uint8_t* frameBuf, uint16_t ledPos, color_t color) {
+    color.red = ((uint16_t)color.red * display_currentBrightness) / 255;
+    color.green = ((uint16_t)color.green * display_currentBrightness) / 255;
+    color.blue = ((uint16_t)color.blue * display_currentBrightness) / 255;
+
+    frameBuf[ledPos * 12 + 3] = ws281x_bit_patterns[(color.green >> 0) & 0x03];
+    frameBuf[ledPos * 12 + 2] = ws281x_bit_patterns[(color.green >> 2) & 0x03];
+    frameBuf[ledPos * 12 + 1] = ws281x_bit_patterns[(color.green >> 4) & 0x03];
+    frameBuf[ledPos * 12 + 0] = ws281x_bit_patterns[(color.green >> 6) & 0x03];
+    
+    frameBuf[ledPos * 12 + 7] = ws281x_bit_patterns[(color.red >> 0) & 0x03];
+    frameBuf[ledPos * 12 + 6] = ws281x_bit_patterns[(color.red >> 2) & 0x03];
+    frameBuf[ledPos * 12 + 5] = ws281x_bit_patterns[(color.red >> 4) & 0x03];
+    frameBuf[ledPos * 12 + 4] = ws281x_bit_patterns[(color.red >> 6) & 0x03];
+    
+    frameBuf[ledPos * 12 + 11] = ws281x_bit_patterns[(color.blue >> 0) & 0x03];
+    frameBuf[ledPos * 12 + 10] = ws281x_bit_patterns[(color.blue >> 2) & 0x03];
+    frameBuf[ledPos * 12 + 9] = ws281x_bit_patterns[(color.blue >> 4) & 0x03];
+    frameBuf[ledPos * 12 + 8] = ws281x_bit_patterns[(color.blue >> 6) & 0x03];
+}
+
+void display_setDecimalPointAt(uint8_t* frameBuf, uint16_t charPos, uint8_t state, color_t color) {
+    uint32_t fb_base_i = display_calculateFrameBufferCharacterIndex(charPos);
+
+    if (state) {
+        display_setLEDColor(&frameBuf[fb_base_i], 48, color);
+    } else {
+        display_setLEDColor(&frameBuf[fb_base_i], 48, OFF);
+    }
+}
+
+void display_setCharDataAt(uint8_t* frameBuf, uint16_t charPos, uint16_t charData, color_t color) {
+    uint32_t fb_base_i = display_calculateFrameBufferCharacterIndex(charPos);
+    memset(&frameBuf[fb_base_i], 0x88, DIV_CEIL(CONFIG_DISPLAY_FRAMEBUF_BITS_PER_CHAR, 8));
+    
+    if (charData & A) {
+        display_setLEDColor(&frameBuf[fb_base_i], 0, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 1, color);
+    }
+    if (charData & B) {
+        display_setLEDColor(&frameBuf[fb_base_i], 2, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 3, color);
+    }
+    if (charData & C) {
+        display_setLEDColor(&frameBuf[fb_base_i], 4, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 5, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 6, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 7, color);
+    }
+    if (charData & D) {
+        display_setLEDColor(&frameBuf[fb_base_i], 8, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 9, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 10, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 11, color);
+    }
+    if (charData & E) {
+        display_setLEDColor(&frameBuf[fb_base_i], 12, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 13, color);
+    }
+    if (charData & F) {
+        display_setLEDColor(&frameBuf[fb_base_i], 14, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 15, color);
+    }
+    if (charData & G) {
+        display_setLEDColor(&frameBuf[fb_base_i], 16, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 17, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 18, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 19, color);
+    }
+    if (charData & H) {
+        display_setLEDColor(&frameBuf[fb_base_i], 20, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 21, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 22, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 23, color);
+    }
+    if (charData & K) {
+        display_setLEDColor(&frameBuf[fb_base_i], 24, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 25, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 26, color);
+    }
+    if (charData & M) {
+        display_setLEDColor(&frameBuf[fb_base_i], 27, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 28, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 29, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 30, color);
+    }
+    if (charData & N) {
+        display_setLEDColor(&frameBuf[fb_base_i], 31, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 32, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 33, color);
+    }
+    if (charData & R) {
+        display_setLEDColor(&frameBuf[fb_base_i], 45, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 46, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 47, color);
+    }
+    if (charData & S) {
+        display_setLEDColor(&frameBuf[fb_base_i], 41, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 42, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 43, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 44, color);
+    }
+    if (charData & T) {
+        display_setLEDColor(&frameBuf[fb_base_i], 38, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 39, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 40, color);
+    }
+    if (charData & U) {
+        display_setLEDColor(&frameBuf[fb_base_i], 36, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 37, color);
+    }
+    if (charData & P) {
+        display_setLEDColor(&frameBuf[fb_base_i], 34, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 35, color);
+    }
+}
+
+void display_charbuf_to_framebuf(uint8_t* charBuf, uint8_t* frameBuf, uint16_t charBufSize, uint16_t frameBufSize) {
+    uint8_t prevWasLetter = 0;
+    uint16_t decPointMergeCnt = 0;
+    uint16_t cb_i_display = 0;
+    color_t color = {255, 0, 128};
+
+    memset(frameBuf, 0x88, frameBufSize);
+
+    for (uint16_t cb_i_source = 0; cb_i_source < charBufSize; cb_i_source++) {
+        if (charBuf[cb_i_source] == '.') {
+            if (!prevWasLetter) {
+                // Current char is . and previous was . too
+                display_setDecimalPointAt(frameBuf, cb_i_display, 1, color);
+            } else {
+                // Current char is . but previous was a letter
+                // Add the point to the previous letter and update the respective counter
+                cb_i_display--;
+                display_setDecimalPointAt(frameBuf, cb_i_display, 1, color);
+                decPointMergeCnt++;
+                prevWasLetter = 0;
+            }
+        } else {
+            // Current char is a letter
+            if (charBuf[cb_i_source] >= char_seg_font_min && charBuf[cb_i_source] <= char_seg_font_max) {
+                display_setCharDataAt(frameBuf, cb_i_display, char_16seg_font[charBuf[cb_i_source] - char_seg_font_min], color);
+            }
+            prevWasLetter = 1;
+        }
+
+        cb_i_display++;
+    }
+}
+
+void display_render_frame(uint8_t* frame, uint8_t* prevFrame, uint16_t frameBufSize) {
+    if (display_transferOngoing) return;
+
+    ESP_LOGV(LOG_TAG, "Rendering frame:");
+    //ESP_LOG_BUFFER_HEX(LOG_TAG, frame, frameBufSize);
+    spi_transaction_t spi_trans = {
+        .length = (uint32_t)frameBufSize * 8,
+        .tx_buffer = frame,
+    };
+    ESP_ERROR_CHECK(spi_device_transmit(spi, &spi_trans));
+    ets_delay_us(350); // Ensure reset pulse
+}
+
+#endif

components/driver_display_char_16seg_led_ws281x/char_16seg_led_ws281x.h
@@ -1,0 +1,22 @@
+#pragma once
+
+#include "esp_system.h"
+
+#define WS281X_NUM_LEDS (DISPLAY_CHARBUF_SIZE * 49)
+
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} color_t;
+
+void display_init();
+void display_pre_transfer_cb(spi_transaction_t *t);
+void display_post_transfer_cb(spi_transaction_t *t);
+esp_err_t display_set_brightness(uint8_t brightness);
+uint32_t display_calculateFrameBufferCharacterIndex(uint16_t charPos);
+void display_setLEDColor(uint8_t* frameBuf, uint16_t ledPos, color_t color);
+void display_setDecimalPointAt(uint8_t* frameBuf, uint16_t charPos, uint8_t state, color_t color);
+void display_setCharDataAt(uint8_t* frameBuf, uint16_t charPos, uint16_t charData, color_t color);
+void display_charbuf_to_framebuf(uint8_t* charBuf, uint8_t* frameBuf, uint16_t charBufSize, uint16_t frameBufSize);
+void display_render_frame(uint8_t* frame, uint8_t* prevFrame, uint16_t frameBufSize);

components/driver_display_char_16seg_led_ws281x/char_16seg_led_ws281x_old.c
@@ -1,0 +1,208 @@
+/*
+ * Functions for WS281x based LED displays
+ */
+
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <string.h>
+
+#include "macros.h"
+#include "char_16seg_led_ws281x.h"
+#include "util_generic.h"
+#include "char_16seg_font.h"
+#include "driver/rmt.h"
+#include "led_strip.h"
+
+#if defined(CONFIG_DISPLAY_DRIVER_CHAR_16SEG_LED_WS281X)
+
+
+#define LOG_TAG "CH-16SEG-LED-WS281X"
+
+uint8_t display_currentBrightness = 255;
+color_t OFF = { 0, 0, 0 };
+led_strip_t* strip;
+
+
+void display_init() {
+    /*
+     * Set up all needed peripherals
+     */
+
+    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(CONFIG_16SEG_WS281X_DATA_IO, CONFIG_16SEG_WS281X_RMT_CH);
+    config.clk_div = 2;
+    ESP_ERROR_CHECK(rmt_config(&config));
+    ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, ESP_INTR_FLAG_IRAM));
+
+    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(WS281X_NUM_LEDS, (led_strip_dev_t)config.channel);
+    strip = led_strip_new_rmt_ws2812(&strip_config);
+    if (!strip) {
+        ESP_LOGE(LOG_TAG, "Failed to install WS281x driver");
+        return;
+    }
+}
+
+#if defined(CONFIG_DISPLAY_HAS_BRIGHTNESS_CONTROL)
+esp_err_t display_set_brightness(uint8_t brightness) {
+    display_currentBrightness = brightness;
+    return ESP_OK;
+}
+#else
+esp_err_t display_set_brightness(uint8_t brightness) {
+    return ESP_OK;
+}
+#endif
+
+uint32_t display_calculateFrameBufferCharacterIndex(uint16_t charPos) {
+    // Calculate the frame buffer start index for the given character position
+    // Position starts at 0 in the top left corner
+    uint32_t fb_index = 0;
+    fb_index = DIV_CEIL(CONFIG_DISPLAY_FRAMEBUF_BITS_PER_CHAR, 8) * (DISPLAY_CHARBUF_SIZE - charPos - 1);
+    return fb_index;
+}
+
+void display_setLEDColor(uint8_t* frameBuf, uint16_t ledPos, color_t color) {
+    frameBuf[ledPos * 3 + 0] = color.red;
+    frameBuf[ledPos * 3 + 1] = color.green;
+    frameBuf[ledPos * 3 + 2] = color.blue;
+}
+
+void display_setDecimalPointAt(uint8_t* frameBuf, uint16_t charPos, uint8_t state, color_t color) {
+    uint32_t fb_base_i = display_calculateFrameBufferCharacterIndex(charPos);
+
+    if (state) {
+        display_setLEDColor(&frameBuf[fb_base_i], 48, color);
+    } else {
+        display_setLEDColor(&frameBuf[fb_base_i], 48, OFF);
+    }
+}
+
+void display_setCharDataAt(uint8_t* frameBuf, uint16_t charPos, uint16_t charData, color_t color) {
+    uint32_t fb_base_i = display_calculateFrameBufferCharacterIndex(charPos);
+    memset(&frameBuf[fb_base_i], 0x00, 3 * 48);
+    
+    if (charData & A) {
+        display_setLEDColor(&frameBuf[fb_base_i], 0, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 1, color);
+    }
+    if (charData & B) {
+        display_setLEDColor(&frameBuf[fb_base_i], 2, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 3, color);
+    }
+    if (charData & C) {
+        display_setLEDColor(&frameBuf[fb_base_i], 4, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 5, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 6, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 7, color);
+    }
+    if (charData & D) {
+        display_setLEDColor(&frameBuf[fb_base_i], 8, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 9, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 10, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 11, color);
+    }
+    if (charData & E) {
+        display_setLEDColor(&frameBuf[fb_base_i], 12, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 13, color);
+    }
+    if (charData & F) {
+        display_setLEDColor(&frameBuf[fb_base_i], 14, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 15, color);
+    }
+    if (charData & G) {
+        display_setLEDColor(&frameBuf[fb_base_i], 16, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 17, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 18, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 19, color);
+    }
+    if (charData & H) {
+        display_setLEDColor(&frameBuf[fb_base_i], 20, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 21, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 22, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 23, color);
+    }
+    if (charData & K) {
+        display_setLEDColor(&frameBuf[fb_base_i], 24, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 25, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 26, color);
+    }
+    if (charData & M) {
+        display_setLEDColor(&frameBuf[fb_base_i], 27, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 28, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 29, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 30, color);
+    }
+    if (charData & N) {
+        display_setLEDColor(&frameBuf[fb_base_i], 31, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 32, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 33, color);
+    }
+    if (charData & R) {
+        display_setLEDColor(&frameBuf[fb_base_i], 45, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 46, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 47, color);
+    }
+    if (charData & S) {
+        display_setLEDColor(&frameBuf[fb_base_i], 41, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 42, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 43, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 44, color);
+    }
+    if (charData & T) {
+        display_setLEDColor(&frameBuf[fb_base_i], 38, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 39, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 40, color);
+    }
+    if (charData & U) {
+        display_setLEDColor(&frameBuf[fb_base_i], 36, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 37, color);
+    }
+    if (charData & P) {
+        display_setLEDColor(&frameBuf[fb_base_i], 34, color);
+        display_setLEDColor(&frameBuf[fb_base_i], 35, color);
+    }
+}
+
+void display_charbuf_to_framebuf(uint8_t* charBuf, uint8_t* frameBuf, uint16_t charBufSize, uint16_t frameBufSize) {
+    uint8_t prevWasLetter = 0;
+    uint16_t decPointMergeCnt = 0;
+    uint16_t cb_i_display = 0;
+    color_t color = {255, 0, 128};
+
+    memset(frameBuf, 0x00, frameBufSize);
+
+    for (uint16_t cb_i_source = 0; cb_i_source < charBufSize; cb_i_source++) {
+        if (charBuf[cb_i_source] == '.') {
+            if (!prevWasLetter) {
+                // Current char is . and previous was . too
+                display_setDecimalPointAt(frameBuf, cb_i_display, 1, color);
+            } else {
+                // Current char is . but previous was a letter
+                // Add the point to the previous letter and update the respective counter
+                cb_i_display--;
+                display_setDecimalPointAt(frameBuf, cb_i_display, 1, color);
+                decPointMergeCnt++;
+                prevWasLetter = 0;
+            }
+        } else {
+            // Current char is a letter
+            if (charBuf[cb_i_source] >= char_seg_font_min && charBuf[cb_i_source] <= char_seg_font_max) {
+                display_setCharDataAt(frameBuf, cb_i_display, char_16seg_font[charBuf[cb_i_source] - char_seg_font_min], color);
+            }
+            prevWasLetter = 1;
+        }
+
+        cb_i_display++;
+    }
+}
+
+void display_render_frame(uint8_t* frame, uint8_t* prevFrame, uint16_t frameBufSize) {
+    ESP_LOGV(LOG_TAG, "Rendering frame");
+    for (uint16_t i = 0; i < WS281X_NUM_LEDS; i++) {
+        strip->set_pixel(strip, i, ((uint16_t)frame[i * 3 + 0] * display_currentBrightness) / 255, ((uint16_t)frame[i * 3 + 1] * display_currentBrightness) / 255, ((uint16_t)frame[i * 3 + 2] * display_currentBrightness) / 255);
+    }
+    strip->refresh(strip, 100);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+
+#endif

commit dcf6331b46d26c69199f9bc8ec862b905b0305f5

Fix framebuf size error introduced in 7f3ed03 (crude fix)

components/shaders_char/shaders_char.c
@@ -10,6 +10,8 @@
 
 #define LOG_TAG "SHD-CHAR-COLOR"
 
+// TODO: Sweeping Rainbow Shader just turns red on speed > 2???
+
 
 // Order of shaders. This is important!
 // This order needs to match the order in which the shaders

include/macros.h
@@ -47,7 +47,13 @@
     #define DISPLAY_FRAME_TYPE ""
     #define DISPLAY_BUF_SIZE (CONFIG_DISPLAY_WIDTH * CONFIG_DISPLAY_HEIGHT)
     #define DISPLAY_CHARBUF_SIZE (CONFIG_DISPLAY_FRAME_WIDTH * CONFIG_DISPLAY_FRAME_HEIGHT)
+
+    // TODO: Review this. Why is this different?
+    #if defined(CONFIG_DISPLAY_DRIVER_CHAR_16SEG_LED_WS281X)
     #define DISPLAY_FRAMEBUF_SIZE (DISPLAY_CHARBUF_SIZE * DIV_CEIL(CONFIG_DISPLAY_FRAMEBUF_BITS_PER_CHAR, 8))
+    #else
+    #define DISPLAY_FRAMEBUF_SIZE (DIV_CEIL(DISPLAY_CHARBUF_SIZE * CONFIG_DISPLAY_FRAMEBUF_BITS_PER_CHAR, 8))
+    #endif
 #endif
 
 #if defined(CONFIG_DISPLAY_DRIVER_FLIPDOT_LAWO_ALUMA)

sdkconfig.16segled
@@ -640,13 +640,13 @@ CONFIG_HEAP_TRACING_OFF=y
 #
 # Log output
 #
-# CONFIG_LOG_DEFAULT_LEVEL_NONE is not set
+CONFIG_LOG_DEFAULT_LEVEL_NONE=y
 # CONFIG_LOG_DEFAULT_LEVEL_ERROR is not set
 # CONFIG_LOG_DEFAULT_LEVEL_WARN is not set
-CONFIG_LOG_DEFAULT_LEVEL_INFO=y
+# CONFIG_LOG_DEFAULT_LEVEL_INFO is not set
 # CONFIG_LOG_DEFAULT_LEVEL_DEBUG is not set
 # CONFIG_LOG_DEFAULT_LEVEL_VERBOSE is not set
-CONFIG_LOG_DEFAULT_LEVEL=3
+CONFIG_LOG_DEFAULT_LEVEL=0
 # CONFIG_LOG_COLORS is not set
 CONFIG_LOG_TIMESTAMP_SOURCE_RTOS=y
 # CONFIG_LOG_TIMESTAMP_SOURCE_SYSTEM is not set

sdkconfig.krone_splitflap
@@ -640,13 +640,13 @@ CONFIG_HEAP_TRACING_OFF=y
 #
 # Log output
 #
-# CONFIG_LOG_DEFAULT_LEVEL_NONE is not set
+CONFIG_LOG_DEFAULT_LEVEL_NONE=y
 # CONFIG_LOG_DEFAULT_LEVEL_ERROR is not set
 # CONFIG_LOG_DEFAULT_LEVEL_WARN is not set
-CONFIG_LOG_DEFAULT_LEVEL_INFO=y
+# CONFIG_LOG_DEFAULT_LEVEL_INFO is not set
 # CONFIG_LOG_DEFAULT_LEVEL_DEBUG is not set
 # CONFIG_LOG_DEFAULT_LEVEL_VERBOSE is not set
-CONFIG_LOG_DEFAULT_LEVEL=3
+CONFIG_LOG_DEFAULT_LEVEL=0
 # CONFIG_LOG_COLORS is not set
 CONFIG_LOG_TIMESTAMP_SOURCE_RTOS=y
 # CONFIG_LOG_TIMESTAMP_SOURCE_SYSTEM is not set